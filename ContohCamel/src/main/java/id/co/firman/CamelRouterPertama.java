ee27831b9064afe7add6caf133fc8cc3d5c66704
firman hidayat <firmanhidadjat@yahoo.co.id>
2019-11-23 10:25:50 +0700

    Uncomment @Component pada kelas CamelRoutePertama

--- ContohCamel/src/main/java/id/co/firman/CamelRouterPertama.java
+++ ContohCamel/src/main/java/id/co/firman/CamelRouterPertama.java
@@ -1,8 +1,9 @@
 package id.co.firman;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class CamelRouterPertama extends RouteBuilder {
 
 	@Override
